34e6f75df83af18ef7ee9a3e9f02aee255aceef9
leonluong1 <leon1838luong@yahoo.com>
2023-12-20 20:32:32 -0800

    adding sql files for crime and mcdonalds

--- sql/mcdonalds_table.sql
+++ sql/mcdonalds_table.sql
@@ -2,8 +2,9 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/R6eiy7
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+DROP TABLE IF EXISTS mcdonalds;
 
-CREATE TABLE "McDonalds" (
+CREATE TABLE McDonalds (
     "id" SERIAL   NOT NULL,
     "location_num" VARCHAR(20)   NOT NULL,
     "business_name" VARCHAR(64)   NOT NULL,
@@ -11,10 +12,11 @@ CREATE TABLE "McDonalds" (
     "street_address" VARCHAR(64)   NOT NULL,
     "city" VARCHAR(64)   NOT NULL,
     "zipcode" VARCHAR(10)   NOT NULL,
-    "location_desc" VARCHAR(64)   NULL,
-    "location" VARCHAR(64)   NOT NULL,
+    "location_desc" VARCHAR(64)   NOT NULL,
+    "location" VARCHAR(64)   NULL,
     CONSTRAINT "pk_McDonalds" PRIMARY KEY (
         "id"
      )
 );
 
+select * from mcdonalds;

1037670e4afadd0203dd77234a74f878b9cf6562
rabellan <roland.abellano@gmail.com>
2023-12-23 15:19:08 -0800

    cleaned up commented section in logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,25 +1,3 @@
-// // create map here
-// // Downtown LA zoomed at 10
-// let myMap = L.map("map", {
-//   center: [34.02717700409306, -118.2769483261598],
-//   zoom: 12
-// });
-
-// // Add a tile layer.
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(myMap);
-
-// // This is the marker logic below
-// // check mcd_locations array for missing entries
-// mcd_locations.forEach(location => {
-//   if (location.location_1 && location.location_1.latitude && location.location_1.longitude) {
-//     L.marker([location.location_1.latitude, location.location_1.longitude])
-//       .bindPopup(`<h1>${location.dba_name}</h1> <hr> <h4>Zip Code ${location.zip_code}</h4> <h4>Business Owner ${location.business_name}</h4>`)
-//       .addTo(myMap);
-//   }
-// });
-
 // create map here
 // Coordinates of LACMA at 10 zoom
 let myMap = L.map("map", {

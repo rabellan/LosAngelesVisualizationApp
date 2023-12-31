e655f24d02bc494a5b6c4ddbe562d2d907f4534b
leonluong1 <leon1838luong@yahoo.com>
2023-12-31 15:09:57 -0800

    simplifying mcdonalds sql

--- sql/mcdonalds_table.sql
+++ sql/mcdonalds_table.sql
@@ -2,7 +2,26 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/R6eiy7
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- DROP TABLE IF EXISTS mcdonalds;
+DROP TABLE IF EXISTS mcdonalds;
+
+CREATE TABLE McDonalds (
+    "id" SERIAL   NOT NULL,
+    "location_num" VARCHAR(20)   NOT NULL,
+    "business_name" VARCHAR(64)   NOT NULL,
+    "dba_name" VARCHAR(64)   NOT NULL,
+    "street_address" VARCHAR(64)   NOT NULL,
+    "city" VARCHAR(64)   NOT NULL,
+    "zipcode" VARCHAR(10)   NOT NULL,
+    "location_desc" VARCHAR(64)   NOT NULL,
+    "lat" NUMERIC(7,4)   NULL,
+	"lon" NUMERIC(7,4)   NULL,
+    CONSTRAINT "pk_McDonalds" PRIMARY KEY (
+        "id"
+     )
+);
+
+select * from mcdonalds;
+
 
 /*
 CREATE TABLE McDonalds (
@@ -19,7 +38,9 @@ CREATE TABLE McDonalds (
         "id"
      )
 );
+*/
 
+/*
 ALTER TABLE mcdonalds
 ADD COLUMN lat NUMERIC(7,4),
 ADD COLUMN lon NUMERIC(7,4);
@@ -45,13 +66,19 @@ ALTER TABLE mcdonalds
 DROP COLUMN location;
 */
 
-
+/*
 select id, substring(split_part(location, ',', 1), 2) from mcdonalds;
 select id, substring(split_part(location, ',', 2), 1, length(split_part(location, ',', 2)) - 1) from mcdonalds;
+*/
 
-select * from mcdonalds;
 
+/*
+PSQL COMMAND
 
+psql -d la_crime_db -U postgres -c 
+"\copy (select * from mcdonalds) TO 'C:\Users\leon1\OneDrive\bootcamp_hw\group_projects\LosAngelesVisualizationApp\static\resources\mcdonalds_clean.csv' 
+WITH DELIMITER ',' CSV HEADER;"
+*/
 
 
 

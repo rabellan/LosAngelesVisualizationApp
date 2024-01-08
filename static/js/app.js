ac36477a0b51163497a01480fec9ef196cb5845a
leonluong1 <leon1838luong@yahoo.com>
2024-01-07 19:14:01 -0800

    making changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//const port = process.env.PORT || 3000;
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 10000;
 
 const {
     queryCrime,
